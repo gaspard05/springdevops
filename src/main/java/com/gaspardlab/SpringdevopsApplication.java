1f48003d7e9c5b0eaf6bb455be16b8af4c5a77a8
Gaspard Nzamurambaho <gnzamura@hotmail.com>
2017-11-06 02:53:39 +0000

    Setting up spring boot application with HelloWorldController

--- src/main/java/com/gaspardlab/SpringdevopsApplication.java
+++ src/main/java/com/gaspardlab/SpringdevopsApplication.java
@@ -1,4 +1,4 @@
-package com.gaspardlab.springdevops;
+package com.gaspardlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
