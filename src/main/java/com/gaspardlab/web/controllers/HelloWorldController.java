37e0c3c1acee41c5260e5d1800bcce181fe7f013
Gaspard Nzamurambaho <gnzamura@hotmail.com>
2017-11-06 03:38:22 +0000

    Added Bootstrap and jquery to pom with webjars

--- src/main/java/com/gaspardlab/web/controllers/HelloWorldController.java
+++ src/main/java/com/gaspardlab/web/controllers/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.gaspardlab.controllers;
+package com.gaspardlab.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
