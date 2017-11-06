89c1a516932d01e39d04165d32d0399fea07f4e8
Gaspard Nzamurambaho <gnzamura@hotmail.com>
2017-11-06 02:43:17 +0000

    Initial Commit

---
+++ src/main/java/com/gaspardlab/springdevops/SpringdevopsApplication.java
@@ -0,0 +1,12 @@
+package com.gaspardlab.springdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdevopsApplication.class, args);
+	}
+}
